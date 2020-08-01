88ba0fbd534aac8afb55e1c7bb9515f861e4fafb
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-08-01 19:21:08 +0100

    Fetch for Dead and Unknown characters
    Return list element for each character returned

--- src/components/Dead.js
+++ src/components/Dead.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { useQuery } from "react-query";
+
+const fetchDead = async () => {
+  const res = await fetch(
+    "https://rickandmortyapi.com/api/character/?status=dead"
+  );
+  return res.json();
+};
 
 function Dead() {
+  const { data, status } = useQuery("dead", fetchDead);
+
   return (
     <>
       <h2>Dead</h2>
+      {status === "error" && <p>There was an error fetching the data.</p>}
+      {status === "loading" && <p>Loading the data...</p>}
+      {status === "success" && (
+        <ul>
+          {/* data.results returns some duplicate characters so need a new object with just the unique character names */}
+          {[...new Set(data.results.map((character) => character.name))].map(
+            (name) => (
+              <li key={name}>{name}</li>
+            )
+          )}
+        </ul>
+      )}
     </>
   );
 }
--- src/components/Unknown.js
+++ src/components/Unknown.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { useQuery } from "react-query";
+
+const fetchUnknown = async () => {
+  const res = await fetch(
+    "https://rickandmortyapi.com/api/character/?status=unknown"
+  );
+  return res.json();
+};
 
 function Unknown() {
+  const { data, status } = useQuery("unknown", fetchUnknown);
+
   return (
     <>
       <h2>Unknown</h2>
+      {status === "error" && <p>There was an error fetching the data.</p>}
+      {status === "loading" && <p>Loading the data...</p>}
+      {status === "success" && (
+        <ul>
+          {/* data.results returns some duplicate characters so need a new object with just the unique character names */}
+          {[...new Set(data.results.map((character) => character.name))].map(
+            (name) => (
+              <li key={name}>{name}</li>
+            )
+          )}
+        </ul>
+      )}
     </>
   );
 }

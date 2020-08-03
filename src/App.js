7fb415e2efda732beac19acb46dc4208fc3a198e
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-08-03 10:18:16 +0100

    Add ReactQueryDevtools

--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@ import React from "react";
 import Alive from "./components/Alive";
 import Dead from "./components/Dead";
 import Unknown from "./components/Unknown";
+import { ReactQueryDevtools } from "react-query-devtools";
 
 function App() {
   const [page, setPage] = React.useState("Alive");
 
   return (
-    <div className="App">
-      <h1>Ricky and Morty Characters</h1>
-      <nav>
-        <button onClick={() => setPage("Alive")}>Alive</button>
-        <button onClick={() => setPage("Dead")}>Dead</button>
-        <button onClick={() => setPage("Unknown")}>Unknown</button>
-      </nav>
-      <main>
-        {page === "Alive" && <Alive />}
-        {page === "Dead" && <Dead />}
-        {page === "Unknown" && <Unknown />}
-      </main>
-    </div>
+    <>
+      <div className="App">
+        <h1>Ricky and Morty Characters</h1>
+        <nav>
+          <button onClick={() => setPage("Alive")}>Alive</button>
+          <button onClick={() => setPage("Dead")}>Dead</button>
+          <button onClick={() => setPage("Unknown")}>Unknown</button>
+        </nav>
+        <main>
+          {page === "Alive" && <Alive />}
+          {page === "Dead" && <Dead />}
+          {page === "Unknown" && <Unknown />}
+        </main>
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
   );
 }
 
